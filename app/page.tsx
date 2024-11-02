88839af8ed03fe86fc65402e9056a9f957dc0988
technologi-service <advancedinthefuture@gmail.com>
2024-11-02 20:14:33 +0100

    add step for documentacion

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import DialogWelcomePage from "@/components/dialogos/dialogoDocumentacion";
 
-export default function Home() {
+export default function HomeTikeo() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>hello app ya se pueden ver los cambios ordenados</h1>
-      <Button>MORE info</Button>
-    </div>
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <DialogWelcomePage />
+      </div>
+    </>
   );
 }
